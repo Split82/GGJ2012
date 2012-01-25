c7e27402428d37d9067ed13664babb89313c73f6
Jan Ilavsky <hyperbolicmagnetism@gmail.com>
2012-01-26 00:16:17 +0100

    Initial Commit

---
+++ GGJ2012/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  GGJ2012
+//
+//  Created by Jan Ilavsky on 1/26/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ GGJ2012/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  GGJ2012
+//
+//  Created by Jan Ilavsky on 1/26/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
