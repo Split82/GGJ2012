ba191075e974f7e39b387489e14fbe616d8ac172
Lukas Foldyna <lukas.foldyna@arat.cz>
2012-01-28 02:04:09 +0100

    Added capsule view for mixer

---
+++ GGJ2012/MixerCapsuleView.m
@@ -0,0 +1,24 @@
+//
+//  MixerCapsuleView.m
+//  GGJ2012
+//
+//  Created by Lukáš Foldýna on 28.01.12.
+//  Copyright (c) 2012 Hyperbolic Magnetism. All rights reserved.
+//
+
+#import "MixerCapsuleView.h"
+
+
+@implementation MixerCapsuleView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+ 
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
